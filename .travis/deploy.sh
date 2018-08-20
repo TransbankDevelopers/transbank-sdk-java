bf615628979989474d403cbd81c2938f3a7d2385
afiebig <alfredo.fiebig@continuum.cl>
2018-08-20 17:57:43 -0300

    Update deploy script to check the tag name

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+TRAVIS_TAG=v2.0.0
+
 if [ ! -z "$TRAVIS_TAG" ]
 then
-    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    echo "Tag found"
+    if ( echo $TRAVIS_TAG | egrep -i '^v[0-9]+\.[0-9]+\.[0-9]+')
+    then
+        VERSION_NUMBER=${TRAVIS_TAG:1}
+        echo "on a tag -> set pom.xml <version> to $VERSION_NUMBER"
+        mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    else
+        echo "Tag does not start with v: ${TRAVIS_TAG} keep snapshot version in pom.xml"
+    fi
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi

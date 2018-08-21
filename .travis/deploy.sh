5f873779512b40d67baba0f8919331ef38df893b
afiebig <alfredo.fiebig@continuum.cl>
2018-08-21 10:10:13 -0300

    Fix wrong tag variable, and deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,11 +7,11 @@ then
     then
         VERSION_NUMBER=${TRAVIS_TAG:1}
         echo "on a tag -> set pom.xml <version> to $VERSION_NUMBER"
-        mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+        mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$VERSION_NUMBER 1>/dev/null 2>/dev/null
     else
         echo "Tag does not start with v: ${TRAVIS_TAG} keep snapshot version in pom.xml"
     fi
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
 else
     echo "not on a tag no deploy trigered"
 fi

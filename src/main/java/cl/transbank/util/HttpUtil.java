14ce5dad9028bbbd956a8eb9f3a2e7444d05c14e
Gonzalo Castillo Fajardo <goncafa@gmail.com>
2019-06-25 11:45:00 -0400

    Implement Transaction Commit for Webpay Plus

--- src/main/java/cl/transbank/util/HttpUtil.java
+++ src/main/java/cl/transbank/util/HttpUtil.java
@@ -9,14 +9,14 @@ import java.net.URL;
 import java.util.Map;
 
 public interface HttpUtil {
-    <T> T request(@NonNull URL url, RequestMethod method, @NonNull Object request, Map<String, String> headers,
+    <T> T request(@NonNull URL url, RequestMethod method, Object request, Map<String, String> headers,
                   Class<T> clazz) throws IOException;
     String request(URL url, HttpUtilImpl.RequestMethod method, String query) throws IOException;
     String request(URL url, HttpUtilImpl.RequestMethod method, String query, HttpUtil.ContentType contentType)
             throws IOException;
-    String request(@NonNull URL url, RequestMethod method, @NonNull String query, Map<String, String> headers)
+    String request(@NonNull URL url, RequestMethod method, String query, Map<String, String> headers)
             throws IOException;
-    String request(@NonNull URL url, RequestMethod method, @NonNull String query, ContentType contentType, Map<String,
+    String request(@NonNull URL url, RequestMethod method, String query, ContentType contentType, Map<String,
             String> headers) throws IOException;
 
     @AllArgsConstructor

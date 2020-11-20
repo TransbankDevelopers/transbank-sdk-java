684874bbaa1be0743c28439531e57dafb104493d
Felipe Fiebig <felipe.fiebig@continuum.cl>
2020-11-20 15:51:50 -0300

    Add capture request

--- src/main/java/cl/transbank/webpay/oneclick/TransactionCaptureRequest.java
+++ src/main/java/cl/transbank/webpay/oneclick/TransactionCaptureRequest.java
@@ -1,13 +1,19 @@
 package cl.transbank.webpay.oneclick;
 
+import com.google.gson.annotations.SerializedName;
+
 import cl.transbank.model.WebpayApiRequest;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@NoArgsConstructor
+@AllArgsConstructor
 @Getter @Setter @ToString
 class TransactionCaptureRequest extends WebpayApiRequest {
-    //TODO VALIDATE REQUEST FOR PROPERTIES WHEN WE HAVE DOC
+    @SerializedName("commerce_code") private String commerceCode;
+    @SerializedName("buy_order") private String buyOrder;
+    @SerializedName("authorization_code") private String authorizationCode;
+    @SerializedName("capture_amount") private double amount;
 }

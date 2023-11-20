dd4e08310d63a2bb8431e01bd1ca50ad3d038e58
Miguel Vargas <mvarlic@gmail.com>
2023-11-20 10:38:24 -0500

    feat: remove api 1.3 methods from Webpay Plus

--- src/main/java/cl/transbank/webpay/webpayplus/WebpayTransaction.java
+++ src/main/java/cl/transbank/webpay/webpayplus/WebpayTransaction.java
@@ -10,14 +10,10 @@ import cl.transbank.util.ValidationUtil;
 import cl.transbank.util.WebpayApiResource;
 import cl.transbank.webpay.exception.*;
 import cl.transbank.webpay.common.TransactionCaptureRequest;
-import cl.transbank.webpay.requests.*;
-import cl.transbank.webpay.responses.*;
 import cl.transbank.webpay.webpayplus.requests.TransactionCreateRequest;
 import cl.transbank.webpay.common.TransactionRefundRequest;
 import cl.transbank.webpay.webpayplus.responses.*;
-
 import java.io.IOException;
-import java.util.List;
 
 abstract class WebpayTransaction extends BaseTransaction {
 
@@ -85,52 +81,4 @@ abstract class WebpayTransaction extends BaseTransaction {
         }
     }
 
-    public IncreaseAmountResponse increaseAmount(String token, String buyOrder, String authorizationCode, double amount) throws IOException, IncreaseAmountException {
-        ValidationUtil.hasTextWithMaxLength(token, ApiConstants.TOKEN_LENGTH, "token");
-        ValidationUtil.hasTextWithMaxLength(buyOrder, ApiConstants.BUY_ORDER_LENGTH, "buyOrder");
-        ValidationUtil.hasTextWithMaxLength(authorizationCode, ApiConstants.AUTHORIZATION_CODE_LENGTH, "authorizationCode");
-        WebpayApiRequest request = new IncreaseAmountRequest(options.getCommerceCode(), buyOrder, authorizationCode, amount);
-        String endpoint = String.format("%s/transactions/%s/amount", ApiConstants.WEBPAY_ENDPOINT, token);
-        try {
-            return WebpayApiResource.execute(endpoint, HttpUtil.RequestMethod.PUT, request, options, IncreaseAmountResponse.class);
-        } catch (TransbankException e) {
-            throw new IncreaseAmountException(e);
-        }
-    }
-
-    public IncreaseAuthorizationDateResponse increaseAuthorizationDate(String token, String buyOrder, String authorizationCode) throws IOException, IncreaseAuthorizationDateException {
-        ValidationUtil.hasTextWithMaxLength(token, ApiConstants.TOKEN_LENGTH, "token");
-        ValidationUtil.hasTextWithMaxLength(buyOrder, ApiConstants.BUY_ORDER_LENGTH, "buyOrder");
-        ValidationUtil.hasTextWithMaxLength(authorizationCode, ApiConstants.AUTHORIZATION_CODE_LENGTH, "authorizationCode");
-        WebpayApiRequest request = new IncreaseAuthorizationDateRequest(options.getCommerceCode(), buyOrder, authorizationCode);
-        String endpoint = String.format("%s/transactions/%s/authorization_date", ApiConstants.WEBPAY_ENDPOINT, token);
-        try {
-            return WebpayApiResource.execute(endpoint, HttpUtil.RequestMethod.PUT, request, options, IncreaseAuthorizationDateResponse.class);
-        } catch (TransbankException e) {
-            throw new IncreaseAuthorizationDateException(e);
-        }
-    }
-
-    public ReversePreAuthorizedAmountResponse reversePreAuthorizedAmount(String token, String buyOrder, String authorizationCode, double amount) throws IOException, ReversePreAuthorizedAmountException {
-        ValidationUtil.hasTextWithMaxLength(token, ApiConstants.TOKEN_LENGTH, "token");
-        ValidationUtil.hasTextWithMaxLength(buyOrder, ApiConstants.BUY_ORDER_LENGTH, "buyOrder");
-        ValidationUtil.hasTextWithMaxLength(authorizationCode, ApiConstants.AUTHORIZATION_CODE_LENGTH, "authorizationCode");
-        WebpayApiRequest request = new ReversePreAuthorizedAmountRequest(options.getCommerceCode(), buyOrder, authorizationCode, amount);
-        String endpoint = String.format("%s/transactions/%s/reverse/amount", ApiConstants.WEBPAY_ENDPOINT, token);
-        try {
-            return WebpayApiResource.execute(endpoint, HttpUtil.RequestMethod.PUT, request, options, ReversePreAuthorizedAmountResponse.class);
-        } catch (TransbankException e) {
-            throw new ReversePreAuthorizedAmountException(e);
-        }
-    }
-
-    public List<DeferredCaptureHistoryResponse> deferredCaptureHistory(String token) throws IOException, DeferredCaptureHistoryException {
-        ValidationUtil.hasTextWithMaxLength(token, ApiConstants.TOKEN_LENGTH, "token");
-        String endpoint = String.format("%s/transactions/%s/details", ApiConstants.WEBPAY_ENDPOINT,token);
-        try {
-            return WebpayApiResource.executeToList(endpoint, HttpUtil.RequestMethod.GET, options, DeferredCaptureHistoryResponse[].class);
-        } catch (TransbankException e) {
-            throw new DeferredCaptureHistoryException(e);
-        }
-    }
 }

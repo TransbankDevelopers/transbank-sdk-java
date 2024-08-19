140c902691dcc4979db2015b9506592540f5d1b0
Miguel Vargas <mvarlic@gmail.com>
2024-08-19 14:35:44 -0500

    feat: remove direct and default config methods in webpayplus modal

--- src/main/java/cl/transbank/webpay/modal/WebpayPlusModal.java
+++ src/main/java/cl/transbank/webpay/modal/WebpayPlusModal.java
@@ -1,37 +1,16 @@
 package cl.transbank.webpay.modal;
 
-import cl.transbank.common.IntegrationApiKeys;
-import cl.transbank.common.IntegrationCommerceCodes;
-import cl.transbank.common.IntegrationType;
 import cl.transbank.model.Options;
-import cl.transbank.webpay.common.WebpayOptions;
 
 /**
  * This class provides methods to configure and perform transactions with the PatpassByWebpay service.
  */
 public class WebpayPlusModal {
 
-  private static Options options;
-
   /**
    * This class provides methods to configure and perform transactions with the WebpayPlusModal service.
    */
   public static class Transaction extends WebpayModalTransaction {
-
-    /**
-     * Default constructor. Uses default options if none are provided.
-     */
-    public Transaction() {
-      super(
-        WebpayPlusModal.options != null
-          ? WebpayPlusModal.options
-          : new WebpayOptions(
-            IntegrationCommerceCodes.WEBPAY_PLUS_MODAL,
-            IntegrationApiKeys.WEBPAY,
-            IntegrationType.TEST
-          )
-      );
-    }
 
     /**
      * Constructor with options. Uses provided options.
@@ -42,44 +21,4 @@ public class WebpayPlusModal {
     }
   }
 
-  /*
-    |--------------------------------------------------------------------------
-    | Environment Configuration
-    |--------------------------------------------------------------------------
-    */
-
-  /**
-   * Configures the environment for integration.
-   * @param commerceCode The commerce code.
-   * @param apiKey The API key.
-   */
-  public static void configureForIntegration(
-    String commerceCode,
-    String apiKey
-  ) {
-    options = new WebpayOptions(commerceCode, apiKey, IntegrationType.TEST);
-  }
-
-  public static void configureForProduction(
-    String commerceCode,
-    String apiKey
-  ) {
-    options = new WebpayOptions(commerceCode, apiKey, IntegrationType.LIVE);
-  }
-
-  public static void configureForTesting() {
-    configureForIntegration(
-      IntegrationCommerceCodes.WEBPAY_PLUS_MODAL,
-      IntegrationApiKeys.WEBPAY
-    );
-  }
-
-  public static void configureForMock() {
-    options =
-      new WebpayOptions(
-        IntegrationCommerceCodes.WEBPAY_PLUS_MODAL,
-        IntegrationApiKeys.WEBPAY,
-        IntegrationType.SERVER_MOCK
-      );
-  }
 }

2bb13db6ffd48c4ace33013f9a64ec0bb62fa607
Miguel Vargas <mvarlic@gmail.com>
2024-08-19 14:32:35 -0500

    feat: remove direct and default config methods in oneclick

--- src/main/java/cl/transbank/webpay/oneclick/Oneclick.java
+++ src/main/java/cl/transbank/webpay/oneclick/Oneclick.java
@@ -1,34 +1,16 @@
 package cl.transbank.webpay.oneclick;
 
-import cl.transbank.common.IntegrationApiKeys;
-import cl.transbank.common.IntegrationCommerceCodes;
-import cl.transbank.common.IntegrationType;
 import cl.transbank.model.Options;
-import cl.transbank.webpay.common.WebpayOptions;
 
 /**
  * This class provides methods to configure and perform transactions with the Oneclick service.
  */
 public class Oneclick {
 
-  private static Options options;
-
   /**
    * This class provides methods to perform Oneclick Mall Inscriptions.
    */
   public static class MallInscription extends OneclickMallInscription {
-
-    public MallInscription() {
-      super(
-        Oneclick.options != null
-          ? Oneclick.options
-          : new WebpayOptions(
-            IntegrationCommerceCodes.ONECLICK_MALL,
-            IntegrationApiKeys.WEBPAY,
-            IntegrationType.TEST
-          )
-      );
-    }
 
     public MallInscription(Options options) {
       super(options);
@@ -40,63 +22,9 @@ public class Oneclick {
    */
   public static class MallTransaction extends OneclickMallTransaction {
 
-    public MallTransaction() {
-      super(
-        Oneclick.options != null
-          ? Oneclick.options
-          : new WebpayOptions(
-            IntegrationCommerceCodes.ONECLICK_MALL,
-            IntegrationApiKeys.WEBPAY,
-            IntegrationType.TEST
-          )
-      );
-    }
-
     public MallTransaction(Options options) {
       super(options);
     }
   }
 
-  /*
-    |--------------------------------------------------------------------------
-    | Environment Configuration
-    |--------------------------------------------------------------------------
-    */
-
-  public static void configureForIntegration(
-    String commerceCode,
-    String apiKey
-  ) {
-    options = new WebpayOptions(commerceCode, apiKey, IntegrationType.TEST);
-  }
-
-  public static void configureForProduction(
-    String commerceCode,
-    String apiKey
-  ) {
-    options = new WebpayOptions(commerceCode, apiKey, IntegrationType.LIVE);
-  }
-
-  public static void configureForTesting() {
-    configureForIntegration(
-      IntegrationCommerceCodes.ONECLICK_MALL,
-      IntegrationApiKeys.WEBPAY
-    );
-  }
-
-  public static void configureForTestingDeferred() {
-    configureForIntegration(
-      IntegrationCommerceCodes.ONECLICK_MALL_DEFERRED,
-      IntegrationApiKeys.WEBPAY
-    );
-  }
-
-  public static void configureForMock() {
-    options =
-      new WebpayOptions(
-        IntegrationCommerceCodes.ONECLICK_MALL,
-        IntegrationApiKeys.WEBPAY,
-        IntegrationType.SERVER_MOCK
-      );
-  }
 }

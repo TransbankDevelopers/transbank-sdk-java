985b3278fe67e944b9a9261dfd6a8cc21cb8d43d
Miguel Vargas <mvarlic@gmail.com>
2024-08-19 16:25:35 -0500

    feat: add build method for patpass

--- src/main/java/cl/transbank/patpass/PatpassComercio.java
+++ src/main/java/cl/transbank/patpass/PatpassComercio.java
@@ -1,6 +1,10 @@
 package cl.transbank.patpass;
 
+import cl.transbank.common.IntegrationType;
 import cl.transbank.model.Options;
+import cl.transbank.patpass.model.PatpassOptions;
+import cl.transbank.webpay.common.WebpayOptions;
+import cl.transbank.webpay.webpayplus.WebpayPlus;
 
 /**
  * This class represents the details of a MallTransactionCommit.
@@ -19,6 +23,30 @@ public class PatpassComercio {
     public Inscription(Options options) {
       super(options);
     }
+  }
+
+  /**
+   * Creates and returns an instance of `Inscription` configured for the integration environment.
+   *
+   * @param commerceCode The commerce code.
+   * @param apiKey The API key used for authentication.
+   * @return A new instance of `Inscription` configured for the test environment (IntegrationType.TEST).
+   */
+  public static Inscription buildForIntegration(String commerceCode, String apiKey)
+  {
+    return new Inscription(new PatpassOptions(commerceCode, apiKey, IntegrationType.TEST));
+  }
+
+  /**
+   * Creates and returns an instance of `Inscription` configured for the production environment.
+   *
+   * @param commerceCode The commerce code.
+   * @param apiKey The API key used for authentication.
+   * @return A new instance of `Inscription` configured for the production environment (IntegrationType.LIVE).
+   */
+  public static Inscription buildForProduction(String commerceCode, String apiKey)
+  {
+    return new Inscription(new PatpassOptions(commerceCode, apiKey, IntegrationType.LIVE));
   }
 
 }
